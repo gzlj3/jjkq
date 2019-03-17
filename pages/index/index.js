87187a94db996de28ae41e18c4b36d7df1ed60de
gzlj3 <gzlj3@163.com>
2019-03-17 18:45:49 +0800

    home

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,11 +1,11 @@
-const { CONSTS } = requirePlugin("XXZNZJ")
+const { CONSTS, services } = requirePlugin("XXZNZJ")
 const myBehavior = require('../../xxznzjBehaviors.js')
 const menuList = [
   {
     name: '学员管理',
     open: false,
     page: 'plugin://XXZNZJ/xyglmain'
-  },
+  }, 
   {
     name: '教职工管理',
     open: false,
@@ -34,6 +34,33 @@ Component({
   methods: {
     onLoad: function (options) {
       this.setData({ onLoadOptions: options });
+      const self = this;
+      wx.login({
+        success(res) {
+          if (res.code) {
+            console.log(res.code);
+            // 发起网络请求
+            // wx.request({
+            //   url: 'https://test.com/onLogin',
+            //   data: {
+            //     code: res.code
+            //   }
+            // })
+            const response = services.postData(300, { code: res.code });
+            services.handleAfterRemote(response, '测试服务',
+              (resultData) => {
+                console.log(resultData);
+                const { session_key } = resultData;
+                self.setData({ session_key });
+              }
+            );
+
+          } else {
+            console.log('登录失败！' + res.errMsg)
+          }
+        }
+      })      
+
     },
     onGetUserInfo: function (e) {
       console.log('host get user....:', e);
@@ -41,6 +68,15 @@ Component({
     },
     onGetPhoneNumber: function (e) {
       console.log('host getPhoneNumber:', e);
+
+      const { session_key } = this.data; 
+      const response = services.postData(301, { session_key, phoneData: e.detail });
+      services.handleAfterRemote(response, '测试服务',
+        (resultData) => {
+          console.log(resultData);
+        }
+      );
+
       this.setData({ phoneNumber: e.detail })
     }
   }
